4310f5d25a67f13db50aed406348371e73e0960e
pavels.p <pavels.p@7peakssoftware.com>
2026-01-15 15:21:26 +0700

    Add default args to Windows merge runner

--- merge-two-excels/run_windows.ps1
+++ merge-two-excels/run_windows.ps1
@@ -1,2 +1,6 @@
 $ErrorActionPreference = "Stop"
-py -3.11 bootstrap.py @args
+if ($args.Count -eq 0) {
+  py -3.11 bootstrap.py --template ..\template.xlsx --data ..\data.xlsx --outdir ..\out
+} else {
+  py -3.11 bootstrap.py @args
+}
